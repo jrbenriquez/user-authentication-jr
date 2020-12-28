4570e051465b08dc8cfb99165fa24b60bba8b28d
johnreienriquez <johnrei.enriquez@gmail.com>
2020-12-28 16:51:18 +0800

    removed build folder

--- build/lib/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import AuthorizeUser, signout
-
-urlpatterns = [
-    path('', AuthorizeUser.as_view(), name='authorize'),
-    path('signout/', signout, name='signout')
-]
