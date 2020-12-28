fd933b229dd63b5211cb4322830de3ed1b728422
johnreienriquez <johnrei.enriquez@gmail.com>
2020-12-28 13:16:48 +0800

    updated package files

---
+++ README.md
@@ -0,0 +1,4 @@
+# User Authentication with Token and/or Cookie Support
+
+This is an initial readme.
+Check out github url instead: https://github.com/jrbenriquez/user-authentication-jr
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A token based User model package with support for Cookies and API token authentication",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/jrbenriquez/user-authentication-jr",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
